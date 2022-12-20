32917d940d0944d895bef649cc3383707fc30c68
Alexei Talanov <alexei.talanov003@mail.ru>
2022-12-20 17:51:03 +0300

    Added docker features

--- currency-exchange-service/src/main/java/com/microservices/currencyexchangecervice/CurrencyExchangeServiceApplication.java
+++ currency-exchange-service/src/main/java/com/microservices/currencyexchangecervice/CurrencyExchangeServiceApplication.java
@@ -1,14 +1,13 @@
 package com.microservices.currencyexchangecervice;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CurrencyExchangeCerviceApplication {
+public class CurrencyExchangeServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CurrencyExchangeCerviceApplication.class, args);
+		SpringApplication.run(CurrencyExchangeServiceApplication.class, args);
 	}
 
 }
