08fc87070cbb889b4fc660e01d219b10f6fdb494
Alexei Talanov <alexei.talanov003@mail.ru>
2023-01-22 19:08:17 +0300

    Added logout

--- api-gateway/src/main/java/com/microservices/apigateway/security/SecurityConfig.java
+++ api-gateway/src/main/java/com/microservices/apigateway/security/SecurityConfig.java
@@ -3,17 +3,20 @@ package com.microservices.apigateway.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
 
 
 @Configuration
 public class SecurityConfig {
     @Bean
-    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http, ServerLogoutSuccessHandler handler) {
         http
                     .authorizeExchange()
                     .pathMatchers("/actuator/**", "/", "/currency-exchange/**",
-                            "/currency-converter/**", "/banking/open")
+                            "/currency-converter/**", "/banking/open", "/logout.html", "/")
                     .permitAll()
                 .and()
                     .authorizeExchange()
@@ -21,7 +24,21 @@ public class SecurityConfig {
                     .authenticated()
                 .and()
                     .oauth2Login()
+                .and()
+                    .logout()
+                    .logoutSuccessHandler(handler)
         ;
         return http.build();
+    }
+
+    @Bean
+    public ServerLogoutSuccessHandler keycloakLogoutSuccessHandler(ReactiveClientRegistrationRepository repository) {
+
+        OidcClientInitiatedServerLogoutSuccessHandler oidcLogoutSuccessHandler =
+                new OidcClientInitiatedServerLogoutSuccessHandler(repository);
+
+        oidcLogoutSuccessHandler.setPostLogoutRedirectUri("{baseUrl}/logout.html");
+
+        return oidcLogoutSuccessHandler;
     }
 }
